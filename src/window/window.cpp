48b2c92c4479d61512d428fef1dbf92ee1a111ea
Łukasz Twarduś <ltwardus@gmail.com>
2022-03-04 23:24:34 +0100

    [] Basic image loading done

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -1,5 +1,6 @@
 #include "window.h"
 #include <iostream>
+#include <filesystem>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
@@ -53,7 +54,15 @@ namespace oinkoinkrun::window {
     }
 
     void Window::render() {
-
+        SDL_Texture* texture = IMG_LoadTexture(
+                renderer.get(),
+                (std::filesystem::path("src") / "assets" / "graphics" / "background" / "layer-1-sky.png").c_str());
+        if (!texture) {
+            std::cerr << SDL_GetError() << std::endl;
+            throw std::runtime_error("Cannot load image");
+        }
+        SDL_RenderCopy(renderer.get(), texture, NULL, NULL);
+        SDL_DestroyTexture(texture);
     }
 
     void Window::refresh() {
