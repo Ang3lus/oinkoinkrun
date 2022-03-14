7909207e9214a2b8f97815b565200fc789db9b79
Łukasz Twarduś <ltwardus@gmail.com>
2022-03-14 20:53:01 +0100

    [] small refactoring

--- src/game.cpp
+++ src/game.cpp
@@ -5,10 +5,7 @@
 namespace oinkoinkrun {
     Game::Game() {
         std::cout << "Game::Game" << std::endl;
-    }
 
-    void Game::start() {
-        std::cout << "Game::start" << std::endl;
         scrolling_backgrounds_.emplace_back(
                 window_.load_image(
                         std::filesystem::path("src") / "assets" / "graphics" / "background" / "layer-1-sky.png"),
@@ -21,45 +18,61 @@ namespace oinkoinkrun {
                 window_.load_image(
                         std::filesystem::path("src") / "assets" / "graphics" / "background" / "layer-3-ground.png"),
                 50);
+    }
 
+    void Game::start() {
+        run_ = true;
+
+        std::cout << "Game::start" << std::endl;
         window_.show();
 
-        bool run = true;
         float dt = 0;
         auto prev_tick = std::chrono::steady_clock::now();
         auto next_tick = prev_tick;
-        while(run) {
+        while(run_) {
             dt = std::chrono::duration<float>(next_tick - prev_tick).count();
             prev_tick = next_tick;
-            events_.poll([&run](const events::Events::Event &event) {
-                std::visit([&run](const auto& data) {
-                    if constexpr(std::is_same_v<std::decay_t<decltype(data)>, events::Events::Event::Key>) {
-                        switch (data.code) {
-                            case events::Events::Event::Key::Code::Escape: {
-                                if (data.type == events::Events::Event::Key::Type::Press) {
-                                    std::cout << "ESC quit" << std::endl;
-                                    run = false;
-                                }
-                                break;
-                            }
-                            default:
-                                break;
-                        }
-                    }
-                }, event.data);
-            });
 
-            window_.clear();
-            for (const auto& scrolling_background: scrolling_backgrounds_) {
-                scrolling_background.render(window_);
-            }
-            window_.refresh();
-
-            for (auto& scrolling_background: scrolling_backgrounds_) {
-                scrolling_background.update(dt, window_);
-            }
+            handle_input();
+            render();
+            update(dt);
 
             next_tick = std::chrono::steady_clock::now();
+        }
+    }
+
+    void Game::handle_input() {
+        events_.poll([&](const events::Events::Event &event) {
+            std::visit([&](const auto& data) {
+                if constexpr(std::is_same_v<std::decay_t<decltype(data)>, events::Events::Event::Key>) {
+                    switch (data.code) {
+                        case events::Events::Event::Key::Code::Escape: {
+                            if (data.type == events::Events::Event::Key::Type::Press) {
+                                std::cout << "ESC quit" << std::endl;
+                                run_ = false;
+                            }
+                            break;
+                        }
+                        default:
+                            break;
+                    }
+                }
+            }, event.data);
+        });
+
+    }
+
+    void Game::render() {
+        window_.clear();
+        for (const auto& scrolling_background: scrolling_backgrounds_) {
+            scrolling_background.render(window_);
+        }
+        window_.refresh();
+    }
+
+    void Game::update(float dt) {
+        for (auto& scrolling_background: scrolling_backgrounds_) {
+            scrolling_background.update(dt, window_);
         }
     }
 }
--- src/game.h
+++ src/game.h
@@ -9,6 +9,11 @@ namespace oinkoinkrun {
         Game();
         void start();
     private:
+        void handle_input();
+        void render();
+        void update(float dt);
+
+        bool run_ = false;
         events::Events events_;
         window::Window window_;
         std::vector<graphics::ScrollingBackground> scrolling_backgrounds_;
